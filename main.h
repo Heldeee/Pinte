6ca66c4581b1b9d27b794cc7a89a189b2aa6e5e9
Phu Hung <phu-hung.dang@epita.fr>
2022-01-18 17:36:17 +0100

    ding dang phu hung

---
+++ main.h
@@ -0,0 +1,8 @@
+// for initializing and shutdown functions
+#include <SDL2/SDL.h>
+ 
+// for rendering images and graphics on screen
+#include <SDL2/SDL_image.h>
+ 
+// for using SDL_Delay() functions
+#include <SDL2/SDL_timer.h>
