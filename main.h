b837af02bc467db062cf1f03cab336e5bce16144
Leo DEVIN <leo.devin@epita.fr>
2022-02-13 18:24:28 +0100

    MAINTENANT C PROPRE

--- main.h
+++
@@ -1,8 +0,0 @@
-// for initializing and shutdown functions
-#include <SDL2/SDL.h>
- 
-// for rendering images and graphics on screen
-#include <SDL2/SDL_image.h>
- 
-// for using SDL_Delay() functions
-#include <SDL2/SDL_timer.h>
